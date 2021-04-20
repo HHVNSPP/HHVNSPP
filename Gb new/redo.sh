45e31befb75477fab1b0a3e7d4481312fa79b0a8
Elisa Schaeffer <elisa.schaeffer@gmail.com>
2021-04-20 12:34:01 -0500

    Last(?) figure

---
+++ Gb new/redo.sh
@@ -0,0 +1,5 @@
+gnuplot objfun.plot
+cp objfuna.eps /Users/elisa/Dropbox/Research/Topics/Portfolio/groupBa.eps 
+cp objfunb.eps /Users/elisa/Dropbox/Research/Topics/Portfolio/groupBb.eps
+cp objfunc.eps /Users/elisa/Dropbox/Research/Topics/Portfolio/groupBc.eps
+cp objfund.eps /Users/elisa/Dropbox/Research/Topics/Portfolio/groupBd.eps
