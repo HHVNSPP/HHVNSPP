7b53f5ae2f95c4369662f3e218f330b78b6d54d6
HHVNSPP <madayhq2@gmail.com>
2021-08-30 11:10:45 -0500

    Group C instances

--- Implementation/Generation/generatorSetC.py
+++ Implementation/Generation/generatorSetC.py
@@ -4,15 +4,14 @@ Created on Tue Mar  3 16:06:22 2020
 
 @author: Madys
 """
-import numpy as np
 import random
-projects=[]
 
-sin=1 #si se quieren sinergias 1, si no 0
-num_proj=100
-num_obj=3
+projects=[]
+num_proj=512
+num_obj=4
 num_inst=4
 budget=num_proj*1000
+name="m_o"
 
 def beneficios(obj): 
    if obj==4:
@@ -29,29 +28,27 @@ def beneficios(obj):
        for j in range(4):
           matriz[j]=round(random.randint(10,15)*10*a2[j])
        return matriz
-
-
-if sin==0:
-    name="m3_obj_"
-else:
-    name="m3_sin_4obj_"
     
 for inst in range(num_inst):
-    if sin==0:
-        nsyn=0
-    else:
-        nsyn=random.randint(round(num_proj/20),round(num_proj/15))
-    file1 = open(name+str(num_proj)+"_"+str(inst+1) +".txt", "w") 
-    file1.write(str(num_proj)+"\n")
-    file1.write(str(budget)+"\n")
-    file1.write("2 3 4\n")
-    file1.write(str(nsyn)+"\n")
+    nsyn=random.randint(round(num_proj/20),round(num_proj/15))
+    file = open(name+str(num_proj)+"_"+str(inst+1) +".txt", "w")    
+    file.write(str(num_proj)+"\n")
+    file.write(str(budget)+"\n")
+    file.write("2 3 4\n")
+    file.write(str(nsyn)+"\n")
+    
+    file_ns = open(name+str(num_proj)+"_"+str(inst+1) +"ns.txt", "w")    
+    file_ns.write(str(num_proj)+"\n")
+    file_ns.write(str(budget)+"\n")
+    file_ns.write("2 3 4\n")
+    file_ns.write('0'+"\n")
+    
     for i in range(nsyn):
         npr=random.randint(2,8)
         mina=random.randint(2,npr)
         maxa=random.randint(mina,npr)
         text=str(i)+ " 1 " +str(npr) +" " + str(random.randint(10,30)*10) + " "+ str(mina)+" "+ str(maxa)+"\n"
-        file1.write(text)
+        file.write(text)
         output = list(range(num_proj))
         random.shuffle(output)
         a=""
@@ -60,11 +57,14 @@ for inst in range(num_inst):
                 a=a+str(output[i])+" "
             else:
                 a=a+str(output[i])+"\n"
-        file1.write(a)
+        file.write(a)
     for i in range(num_proj):    
         minimo=random.randint(10,30)*100
         maximo=round(minimo*random.randint(11,15)/10)
-        ben=beneficios()        
-#        file1.write(str(minimo)+" ;"+ str(maximo)+" ;"+ str(random.randint(1,3))+" ;"+str(random.randint(1,2))+" ;"+ str(ben[0])+" ;"+str(ben[1])+" ;"+str(ben[2])+" ;"+str(ben[3])+"\n")
-        file1.write(str(minimo)+" "+ str(maximo)+" "+ str(random.randint(1,3))+" "+str(random.randint(1,2))+" "+ str(ben[0])+" "+str(ben[1])+" "+str(ben[2])+" "+str(ben[3])+" \n")
-    file1.close()   
+        ben=beneficios(num_obj)        
+        cat1=str(random.randint(1,3))
+        cat2= str(random.randint(1,2))
+        file.write(str(minimo)+" "+ str(maximo)+" "+ cat1 +" "+ cat2 +" "+ str(ben[0])+" "+str(ben[1])+" "+str(ben[2])+" "+str(ben[3])+" \n")
+        file_ns.write(str(minimo)+" "+ str(maximo)+" "+ cat1 +" "+ cat2+" "+ str(ben[0])+" "+str(ben[1])+" "+str(ben[2])+" "+str(ben[3])+" \n")
+    file.close()   
+    file_ns.close()  
